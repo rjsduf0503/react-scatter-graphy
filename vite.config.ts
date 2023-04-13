f385937d27c9b6f5955bf05674b1f156766d2f75
류건열 <rjsduf0503@naver.com>
2023-04-14 04:29:26 +0900

    chore: 라이브러리 초기 세팅

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    plugins: [react()],
+  };
+});
